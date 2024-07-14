9a5d043760fd4394be66672bb7d256077f935089
Akhil Bachubhay <akhil.bachubhay@avanade.com>
2024-07-14 14:26:34 -0400

    get nfl scores by week

--- packages/server/src/graphql/resolvers/resolvers.ts
+++ packages/server/src/graphql/resolvers/resolvers.ts
@@ -5,9 +5,24 @@ export const resolvers: IResolvers = {
   hello() {
     return "Hello world!";
   },
-  async nflSchedule({ week }) {
-    const params = { week: week };
-    const schedule = await getNFL("getNFLGamesForWeek", { params: params });
-    return JSON.stringify(schedule.data);
+  async nflScoresWeekly({ week, season }) {
+    const params = { gameWeek: week, season: season };
+    const schedule = await getNFL("getNFLScoresOnly", { params: params });
+    const scheduleData = Object.keys(schedule.data.body).map(key => schedule.data.body[key]);
+
+    return scheduleData
+      .map((game: any) => ({
+        gameId: game.gameID,
+        gameEpoch: new Date(Number(game.gameTime_epoch) * 1000),
+        homeKey: game.home,
+        awayKey: game.away,
+        homeId: game.teamIDHome,
+        awayId: game.teamIDAway,
+        gameStatus: game.gameStatus,
+        awayPts: Number(game.awayPts),
+        homePts: Number(game.homePts),
+        gameClock: game.gameClock
+      }))
+      .sort((a, b) => a.gameEpoch.getTime() - b.gameEpoch.getTime());
   }
 };

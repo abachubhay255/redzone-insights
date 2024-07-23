860dee2db0b8b0130b5031d137027764fbf8e95a
Akhil Bachubhay <akhil.bachubhay@avanade.com>
2024-07-23 00:15:00 -0400

    stop using test data

--- packages/server/src/graphql/resolvers/projections.ts
+++ packages/server/src/graphql/resolvers/projections.ts
@@ -2,7 +2,6 @@ import { sendMessage, uploadFiles } from "#s/openai.js";
 import _ from "lodash";
 import { playerGameLogs } from "./gameLogs.js";
 import { playersByTeam } from "./players.js";
-import testData from "#s/testdata/test.json" assert { type: "json" };
 import { parseJsonResponse, ToFilename } from "./utils.js";
 import { teams } from "./teams.js";
 
@@ -12,20 +11,18 @@ type ProjectionsProps = {
 };
 
 export async function updateProjectionsModel({ homeTeamId, awayTeamId }: ProjectionsProps) {
-  // const homePlayers = await playersByTeam({ teamId: homeTeamId });
-  // const awayPlayers = await playersByTeam({ teamId: awayTeamId });
+  const homePlayers = await playersByTeam({ teamId: homeTeamId });
+  const awayPlayers = await playersByTeam({ teamId: awayTeamId });
 
-  // const players = [...homePlayers, ...awayPlayers];
+  const players = [...homePlayers, ...awayPlayers];
 
-  // const gameLogs = await Promise.all(players.map(async (player: any) => await playerGameLogs({ playerId: player.playerId, games: 5 })));
+  const gameLogs = await Promise.all(players.map(async (player: any) => await playerGameLogs({ playerId: player.playerId, games: 5 })));
 
-  // const logs = gameLogs.filter(log => log && log.length > 0);
-
-  // const logsByPlayer = _.keyBy(logs, "playerId");
+  const logs = gameLogs.filter(log => log && log.length > 0);
 
   let objs = [];
   let names = [];
-  for (const playerLogs of testData) {
+  for (const playerLogs of logs) {
     const playerName = playerLogs[0].playerName;
     const playerId = playerLogs[0].playerId;
     const str = playerName + "_" + playerId;

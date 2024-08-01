bfad4b46717a5f5d85a420f98881757f70854c96
Akhil Bachubhay <akhil.bachubhay@avanade.com>
2024-08-01 03:08:17 -0400

    fix logo

--- packages/client/src/components/AppLogo.tsx
+++ packages/client/src/components/AppLogo.tsx
@@ -3,7 +3,7 @@ import { Group, Image, Text } from "@mantine/core";
 export function AppLogo() {
   return (
     <Group>
-      <Image h={25} src="/favicon.svg" />
+      <Image h={25} src="/assets/logo.svg" />
       <Text fw={600}>
         Redzo
         <Text span style={{ textDecoration: "underline", textDecorationColor: "#c92a2a", textDecorationThickness: 3 }} fw={600}>
